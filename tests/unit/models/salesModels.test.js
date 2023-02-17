aaf152f0b3efd845c43ef4eb32d9b38d9ca7e23e
Arthur de Barros <arthurbarros.nascimento12@gmail.com>
2023-02-17 18:57:38 -0300

    test: tests da model salesprontos

--- tests/unit/models/salesModels.test.js
+++ tests/unit/models/salesModels.test.js
@@ -5,7 +5,12 @@ const { salesModel } = require('../../../src/models');
 
 const connection = require('../../../src/models/connection');
 
-const { productIdFindById } = require('./mocks/sales.model.mock');
+const {
+  productIdFindById,
+  allSalesProducts,
+  findSalesById,
+  findSalesProductById,
+} = require('./mocks/sales.model.mock');
 
 describe('Testando a unidade Model de sales', () => {
   it('Testando se cria um Id para sales', async () => {
@@ -28,6 +33,26 @@ describe('Testando a unidade Model de sales', () => {
     const idProduct = 1;
     const result = await salesModel.searchSalesProductById(idProduct)
     expect(result).to.be.deep.equal(productIdFindById)
+  });
+
+  it('Testando se lista toda sales products', async () => {
+    sinon.stub(connection, 'execute').resolves([allSalesProducts])
+    const result = await salesModel.findAllSalesProducts();
+    expect(result).to.be.deep.equal(allSalesProducts)
+  });
+
+  it('Testando se busca sales pelo id', async () => {
+    sinon.stub(connection, 'execute').resolves([[findSalesById]]);
+    const idSales = 1;
+    const result = await salesModel.findSaleById(idSales);
+    expect(result).to.be.deep.equal(findSalesById);
+  });
+
+  it('Testando se busca sales products pelo id', async () => {
+    sinon.stub(connection, 'execute').resolves([findSalesProductById]);
+    const idSales = 1;
+    const result = await salesModel.findSalesProductById(idSales);
+    expect(result).to.be.deep.equal(findSalesProductById);
   });
 
   afterEach(function () {
