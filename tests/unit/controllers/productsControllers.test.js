dc34c4202ecdf0c380671e8f83710cdc27856afe
Arthur de Barros <arthurbarros.nascimento12@gmail.com>
2023-02-17 20:29:18 -0300

    test: test da controller products feito

---
+++ tests/unit/controllers/productsControllers.test.js
@@ -0,0 +1,30 @@
+const chai = require('chai');
+const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
+
+const { expect } = chai;
+chai.use(sinonChai);
+
+const { productsService } = require('../../../src/services');
+const { productsController } = require('../../../src/controllers')
+const { allProducts } = require('./mock/products.controller.mocks');
+
+describe('Testando a unidade Controller de products', () => {
+  it('Testando se retorna status 200 e a lista de produtos', async () => {
+    const res = {};
+    const req = {};
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns();
+
+    sinon.stub(productsServicem, 'findAll').resolves({ type: null, message: allProducts })
+
+    await productsController.listProducts(req, res);
+
+    expect(res.status).to.have.been.calledWith(200);
+    expect(res.json).to.have.been.calledWith(allProducts);
+  });
+
+  afterEach(function () {
+    sinon.restore();
+  });
+});
