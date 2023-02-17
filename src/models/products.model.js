05c6fbc2caf8d619657892e662361ca127ee05ef
Arthur de Barros <arthurbarros.nascimento12@gmail.com>
2023-02-17 20:18:59 -0300

    test: tests da products Service feitos

--- src/models/products.model.js
+++ src/models/products.model.js
@@ -4,7 +4,6 @@ const findAll = async () => {
   const [result] = await connection.execute(
     'SELECT * FROM StoreManager.products ORDER BY id;',
   );
-  console.log(result);
   return result;
 };
 
--- tests/unit/services/productsServices.test.js
+++ tests/unit/services/productsServices.test.js
@@ -17,7 +17,41 @@ describe('Testando a unidade de Service products', () => {
   });
 
   it('Testando se retorna um erro caso passe um id invalido', async () => {
-    sinon.stub(productsModel, 'findById').resolves()
+    const result = await productsService.findById('a');
+    expect(result.type).to.equal('INVALID_VALUE');
+    expect(result.message).to.equal('"id" must be a number');
+  });
+
+  it('Testando se retorna um error caso o id produto não exista', async () => {
+    sinon.stub(productsModel, 'findById').resolves(undefined);
+    const result = await productsService.findById(1);
+    expect(result.type).to.equal('PRODUCT_NOT_FOUND');
+    expect(result.message).to.equal('Product not found');
+  });
+
+  it('Testando se retorna o produto pelo id', async () => {
+    sinon.stub(productsModel, 'findById').resolves(allProducts[0]);
+    const idProduct = 1;
+    const result = await productsService.findById(idProduct);
+    expect(result.type).to.equal(null);
+    expect(result.message).to.equal(allProducts[0]);
+  });
+
+  it('Testando se retorna um erro se passar nome do tamanho errado', async () => {
+    const name = 'gro';
+    const result = await productsService.createProduct(name);
+    expect(result.type).to.be.equal('INVALID_VALUE');
+    expect(result.message).to.be.equal('"name" length must be at least 5 characters long');
+  });
+
+  it('Testando se cadastra um produto', async () => {
+    const nameProduct = 'Martelo de Thor';
+    sinon.stub(productsModel, 'insert').resolves(nameProduct);
+    sinon.stub(productsModel, 'findById').resolves(allProducts[0]);
+
+    const result = await productsService.createProduct(nameProduct);
+    expect(result.type).to.equal(null);
+    expect(result.message).to.be.equal(allProducts[0]);
   });
 
   afterEach(function () {
